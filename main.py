6850dcbd0848be637a784f0ad1258d6df6321fcd
BountyDev <35352233+BountyDev@users.noreply.github.com>
2020-06-07 19:04:09 -0400

    Basic tk setup

---
+++ main.py
@@ -0,0 +1,12 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter.font import Font
+from PIL import ImageTk,Image
+
+main = Tk()
+
+main.title("Poster")
+main.geometry("640x480")
+C = Canvas(main, bg="white", height=480, width=640)
+
+main.mainloop()
